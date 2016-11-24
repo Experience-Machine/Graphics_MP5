5f05cd45da6568a8d1ddd68b411cbb5a260e787b
ScottDFerguson <sfthe5@gmail.com>
2016-11-24 00:12:56 -0800

    Changed arm segments

--- public_html/src/App/ArmSegment.js
+++ public_html/src/App/ArmSegment.js
@@ -16,43 +16,85 @@ function ArmSegment(shader, name, xPivot, yPivot)
     xf.setPivot(xPivot, yPivot);
     
     // now create the children shapes
-    var obj = new SquareRenderable(shader);  // The yellow 1x2 base
+    var obj = new SquareRenderable(shader);  // The blue shirt
     this.addToSet(obj);
-    obj.setColor([1, 1, 0, 1]);
+    obj.setColor([0, 0, 1, 1]);
     xf = obj.getXform();
     xf.setSize(1, 2);
     xf.setPosition(xPivot, 1 + yPivot);
  
-    obj = new SquareRenderable(shader);  // The red top
+    obj = new SquareRenderable(shader);  // The yellow face
     this.addToSet(obj);
-    obj.setColor([0.7, 0.2, 0.2, 1]);
+    obj.setColor([1 , 1, 0, 1]);
+    xf = obj.getXform();
+    xf.setSize(1.3, 1.3); // so that we can see the connecting point
+    xf.setPosition(xPivot, 2 + yPivot);
+    
+    obj = new SquareRenderable(shader); // The green leg (left)
+    this.addToSet(obj);
+    obj.setColor([0, 1, 0, 1]);
+    xf = obj.getXform();
+    xf.setSize(0.25, .5); // so that we can see the connecting point
+    xf.setPosition(xPivot+0.375, yPivot - .25);
+    
+    obj = new SquareRenderable(shader); // The green leg (right)
+    this.addToSet(obj);
+    obj.setColor([0, 1, 0, 1]);
+    xf = obj.getXform();
+    xf.setSize(0.25, .5); // so that we can see the connecting point
+    xf.setPosition(xPivot-0.375, yPivot - .25);
+    
+    obj = new SquareRenderable(shader); // The left red arm
+    this.addToSet(obj);
+    obj.setColor([1, 0, 0, 1]);
     xf = obj.getXform();
     xf.setSize(1, 0.5); // so that we can see the connecting point
-    xf.setPosition(xPivot, 1.75 + yPivot);
+    xf.setPosition(xPivot - .65, yPivot+1);
     
-    obj = new SquareRenderable(shader); // The green base (left)
+    obj = new SquareRenderable(shader); // The right red arm
     this.addToSet(obj);
-    obj.setColor([0, 1, 0, 1]);
+    obj.setColor([1, 0, 0, 1]);
     xf = obj.getXform();
-    xf.setSize(0.25, 0.25); // so that we can see the connecting point
-    xf.setPosition(xPivot+0.375, yPivot+0.125);
+    xf.setSize(1, 0.5); // so that we can see the connecting point
+    xf.setPosition(xPivot + .65, yPivot+1);
     
-    obj = new SquareRenderable(shader); // The green base (right)
+    obj = new SquareRenderable(shader); // left eye
     this.addToSet(obj);
-    obj.setColor([0, 1, 0, 1]);
+    obj.setColor([0, 0, 0, 1]);
     xf = obj.getXform();
-    xf.setSize(0.25, 0.25); // so that we can see the connecting point
-    xf.setPosition(xPivot-0.375, yPivot+0.125);
+    xf.setSize(.25, .25); // so that we can see the connecting point
+    xf.setPosition(xPivot - .25, yPivot+2.25);
     
-    obj = new SquareRenderable(shader); // The middle blue "circle"
+    obj = new SquareRenderable(shader); // right eye
     this.addToSet(obj);
-    obj.setColor([0, 0, 1, 1]);
+    obj.setColor([0, 0, 0, 1]);
     xf = obj.getXform();
-    xf.setSize(0.5, 0.5); // so that we can see the connecting point
-    xf.setPosition(xPivot, yPivot+1);
+    xf.setSize(.25, 0.25); // so that we can see the connecting point
+    xf.setPosition(xPivot + .25, yPivot+2.25);
+    
+    obj = new SquareRenderable(shader); // left smile block
+    this.addToSet(obj);
+    obj.setColor([0, 0, 0, 1]);
+    xf = obj.getXform();
+    xf.setSize(.15, 0.15); // so that we can see the connecting point
+    xf.setPosition(xPivot - .25, yPivot+1.85);
+    
+    obj = new SquareRenderable(shader); // right smile block
+    this.addToSet(obj);
+    obj.setColor([0, 0, 0, 1]);
+    xf = obj.getXform();
+    xf.setSize(.15, 0.15); // so that we can see the connecting point
+    xf.setPosition(xPivot + .25, yPivot+1.85);
+    
+    obj = new SquareRenderable(shader); // bottom smile block
+    this.addToSet(obj);
+    obj.setColor([0, 0, 0, 1]);
+    xf = obj.getXform();
+    xf.setSize(.45, 0.15); // so that we can see the connecting point
+    xf.setPosition(xPivot, yPivot+1.75);
     
     this.mPulseRate = 0.005;
-    this.mRotateRate = -2;
+    this.mRotateRate = -10;
 }
 gEngine.Core.inheritPrototype(ArmSegment, SceneNode);
 
@@ -60,13 +102,20 @@ ArmSegment.prototype.update = function ()
 {
     SceneNode.prototype.update.call(this);
     
-    // index-1 is the red-top
-    var xf = this.getRenderableAt(1).getXform();
+    var xf = this.getRenderableAt(4).getXform();
+    xf.incRotationByDegree(-this.mRotateRate);
+    
+    var xf = this.getRenderableAt(5).getXform();
     xf.incRotationByDegree(this.mRotateRate);
     
-    // index-4 is the blue circle
-    xf = this.getRenderableAt(4).getXform();
+    xf = this.getRenderableAt(6).getXform();
     xf.incSizeBy(this.mPulseRate);
     if (xf.getWidth() > 0.7 || xf.getWidth() < 0.4)
         this.mPulseRate = -this.mPulseRate;
+    
+    xf = this.getRenderableAt(7).getXform();
+    xf.incSizeBy(this.mPulseRate);
+    if (xf.getWidth() > 0.7 || xf.getWidth() < 0.4)
+        this.mPulseRate = -this.mPulseRate;
+    
 };
